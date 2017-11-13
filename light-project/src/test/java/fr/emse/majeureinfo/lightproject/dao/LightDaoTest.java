84191469ea4d6d2e69244c2f7f062d4ae2326b84
acrepet <agnes.crepet@gmail.com>
2017-11-13 22:02:50 +0100

    fix - change the test database to a dedicated one and fix te DELETE_ALL Operation

--- light-project/src/test/java/fr/emse/majeureinfo/lightproject/dao/LightDaoTest.java
+++ light-project/src/test/java/fr/emse/majeureinfo/lightproject/dao/LightDaoTest.java
@@ -38,7 +38,7 @@ public class LightDaoTest {
 
     protected static final DbSetupTracker TRACKER = new DbSetupTracker();
 
-    private static final Operation DELETE_ALL = DeleteAll.from("light");
+    private static final Operation DELETE_ALL = DeleteAll.from("ROOM","LIGHT","NOISE");
 
     protected void dbSetup(Operation operation) {
         DbSetup setup = new DbSetup(new DataSourceDestination(dataSource),
