b2785e3505c3e4d88db5cdff3d4d579667415c32
Lmmejia11 <lm.mejia11@uniandes.edu.co>
2017-11-15 16:35:46 +0100

    html page

--- src/main/java/fr/emse/majeureinfo/lightproject/controller/RoomController.java
+++ src/main/java/fr/emse/majeureinfo/lightproject/controller/RoomController.java
@@ -3,7 +3,9 @@ package fr.emse.majeureinfo.lightproject.controller;
 import fr.emse.majeureinfo.lightproject.dao.springdao.RoomDao;
 import fr.emse.majeureinfo.lightproject.dto.RoomDto;
 import fr.emse.majeureinfo.lightproject.model.Room;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,31 +24,31 @@ public class RoomController {
         this.roomDao = roomDao;
     }
 
-    @RequestMapping(value="/api/rooms", method = RequestMethod.GET)
+    @GetMapping(value="/api/rooms")
     public List<RoomDto> list() {
         return roomDao.findAll().stream().map(RoomDto::new).collect(Collectors.toList());
     }
 
-    @RequestMapping(value="/api/rooms/{id}", method =RequestMethod.GET)
+    @GetMapping(value="/api/rooms/{id}")
     public RoomDto get(@PathVariable("id") Long id){
         return new RoomDto(roomDao.getOne(id));
     }
 
-    @RequestMapping(value="/api/rooms/{id}/switch-light", method =RequestMethod.GET)
+    @PostMapping(value="/api/rooms/{id}/switch-light")
     public RoomDto switchLight(@PathVariable("id") Long id){
         Room room = roomDao.getOne(id);
         room.switchLight();
         return new RoomDto(room);
     }
 
-    @RequestMapping(value="/api/rooms/{id}/switch-ringer", method =RequestMethod.GET)
+    @PostMapping(value="/api/rooms/{id}/switch-ringer")
     public RoomDto switchRinger(@PathVariable("id") Long id){
         Room room = roomDao.getOne(id);
         room.switchRinger();
         return new RoomDto(room);
     }
 
-    @RequestMapping(value="/api/rooms/on", method =RequestMethod.GET)
+    @GetMapping(value="/api/rooms/on")
     public List<RoomDto> listWithOnLight() {
         return roomDao.findWithOnLight().stream().map(RoomDto::new).collect(Collectors.toList());
     }
