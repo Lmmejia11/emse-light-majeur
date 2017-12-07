e89404043438ce0241b3eee5923f41aeecaa2d36
Lmmejia11 <lm.mejia11@uniandes.edu.co>
2017-12-07 09:42:47 +0100

    modify dto to new, simplerer, requirements

--- src/main/java/fr/emse/majeureinfo/lightproject/dao/BuildingDaoImpl.java
+++ src/main/java/fr/emse/majeureinfo/lightproject/dao/BuildingDaoImpl.java
@@ -1,6 +1,12 @@
 package fr.emse.majeureinfo.lightproject.dao;
 
+import fr.emse.majeureinfo.lightproject.dto.BuildingDetailDto;
+import fr.emse.majeureinfo.lightproject.model.Building;
 import fr.emse.majeureinfo.lightproject.model.Light;
+import org.hibernate.Session;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -12,12 +18,40 @@ public class BuildingDaoImpl {
     @PersistenceContext
     private EntityManager em;
 
-    public List<Light> listBuildings() {
+    public List<Building> listBuildingsLightOn() {
 
         String jpql = "select bd from Building bd where lt.status = :value";
-        TypedQuery<Light> query = em.createQuery(jpql, Light.class);
+        TypedQuery<Building> query = em.createQuery(jpql, Building.class);
         return query.setParameter("value", Light.Status.ON)
                 .getResultList();
     }
+
+    public List<BuildingDetailDto> listBuildingsDetails() {
+
+        String jpql = "select db.id as id, db.name as name, count(db.rooms) as n_rooms from Building bd group by db.id";
+        TypedQuery<BuildingDetailDto> query = em.createQuery(jpql, BuildingDetailDto.class);
+        return query.getResultList();
+    }
+
+    /*public List<BuildingDetailDto> listBuildingsDetails() {
+
+        DetachedCriteria queryLightOn = DetachedCriteria.forClass(Building.class)
+                .setProjection( Projections.rowCount() )
+                .add( Restrictions.eq("rooms.light.status", Light.Status.ON) );
+        DetachedCriteria queryNoiseOn = DetachedCriteria.forClass(Building.class)
+                .setProjection( Projections.rowCount() )
+                .add( Restrictions.eq("rooms.noise.status", Light.Status.ON) );
+
+        TypedQuery<BuildingDetailDto> query = session.createCriteria(Building.class)
+                .setProjection( Projections.projectionList()
+                        .add( Projections.rowCount() )
+                        .add(queryLightOn)
+                        .add(queryNoiseOn)
+                        .add( Projections.groupProperty("rooms.id") )
+                )
+                .setProjection( Projections.alias( Projections.groupProperty(""), "colr" ) )
+        return query.setParameter("value", Light.Status.ON)
+                .getResultList();
+    }*/
 
 }
--- src/main/java/fr/emse/majeureinfo/lightproject/dto/BuildingDetailDto.java
+++ src/main/java/fr/emse/majeureinfo/lightproject/dto/BuildingDetailDto.java
@@ -11,28 +11,11 @@ public class BuildingDetailDto {
     private Long id;
     private String name;
     private int n_rooms;
-    private int n_lightOn;
-    private int n_noiseOn;
 
     public BuildingDetailDto(Building building){
         this.id = building.getId();
         this.name = building.getName();
-        this.n_rooms = 0;
-        this.n_lightOn = 0;
-        this.n_noiseOn = 0;
-        calculateDetails(building.getRooms());
-    }
-    private void calculateDetails(List<Room> rooms ){
-
-        for (Room r:rooms){
-            if (r.getLight().getStatus() == Light.Status.ON) {
-                n_lightOn += 1;
-            }
-            if (r.getLight().getStatus() == Light.Status.ON) {
-                n_noiseOn += 1;
-            }
-            n_rooms += 1;
-        }
+        this.n_rooms = building.getRooms().size() ;
     }
 
     public Long getId() {
@@ -45,13 +28,5 @@ public class BuildingDetailDto {
 
     public int getN_rooms() {
         return n_rooms;
-    }
-
-    public int getN_lightOn() {
-        return n_lightOn;
-    }
-
-    public int getN_noiseOn() {
-        return n_noiseOn;
     }
 }
